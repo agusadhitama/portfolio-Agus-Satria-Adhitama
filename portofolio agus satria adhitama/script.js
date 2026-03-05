b0bd0629f8b4cf32919941f684f3638023cc92ba
Agus Satria Adhitama <55218588+agusadhitama@users.noreply.github.com>
2026-03-05 15:02:28 +0700

    Add files via upload

---
+++ portofolio agus satria adhitama/script.js
@@ -0,0 +1,18 @@
+var typed = new Typed("#typing", {
+  strings: [
+    "Web Developer",
+    "IT Support Engineer",
+    "Network Enthusiast",
+    "Tech Problem Solver"
+  ],
+  typeSpeed: 60,
+  backSpeed: 40,
+  loop: true
+});
+ScrollReveal().reveal('.project', {
+  distance: '40px',
+  duration: 1000,
+  easing: 'ease-in-out',
+  origin: 'bottom',
+  interval: 200
+});
